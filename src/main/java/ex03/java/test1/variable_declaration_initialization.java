6faaeb6ed4464cbdaca278fe9d74a784cfc97ee4
indranilkanawade <ijkanawade@gmail.com>
2025-06-23 22:14:51 +0530

    variable declaration and initialization

---
+++ src/main/java/ex03/java/test1/variable_declaration_initialization.java
@@ -0,0 +1,14 @@
+package ex03.java.test1;
+
+public class variable_declaration_initialization {
+    public static void main(String[] args) {
+        int a =25;
+        double d = 3.14;
+        boolean b = true;
+        char ch = 'A';
+        System.out.println("Integer: "+a);
+        System.out.println("Double: "+d);
+        System.out.println("Boolean: "+b);
+        System.out.println("Character: "+ch);
+    }
+}
