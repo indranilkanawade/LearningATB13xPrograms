bfe976eeade68a5c309dfa022e0c729e10b8b8b0
indranilkanawade <ijkanawade@gmail.com>
2025-06-23 23:21:14 +0530

    print numbers

---
+++ src/main/java/ex01/java/basics/test.java
@@ -0,0 +1,14 @@
+package ex01.java.basics;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(10+20+"Hello");
+        int a =5;
+        System.out.println(++a);
+        if(a==6){
+
+        }else if(a==7){
+
+        }
+    }
+}
